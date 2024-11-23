5f964845ac2f8db75b1dcd8c71ac92ea77f434b5
Hoven <hovenyang.best@gmail.com>
2024-11-23 13:49:31 +0800

    feat: add new game type

--- domain/shared/types.go
+++ domain/shared/types.go
@@ -7,12 +7,15 @@ const (
 	ChineseEightBall
 	Snooker
 	Genting
+	NineBall
 )
 
 func (gt BilliardGameType) String() string {
 	switch gt {
 	case ChineseEightBall:
 		return "中式八球"
+	case NineBall:
+		return "九球追分"
 	case Snooker:
 		return "斯诺克"
 	case Genting:
